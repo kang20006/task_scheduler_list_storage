288012b267774880f88d0b736df4f5ba697c7531
wen_kang <kang20006@gmail.com>
2023-06-01 10:40:02 +0800

    comment from python script

--- transform.py
+++ transform.py
@@ -68,7 +68,7 @@ PATH_OF_GIT_REPO =os.path.join(dir_path, '.git')  # make sure .git folder is pro
 COMMIT_MESSAGE = 'comment from python script'
 def git_push():
     repo = Repo(PATH_OF_GIT_REPO)
-    repo.git.add(update=True)
+    repo.git.add(A=True)
     repo.index.commit(COMMIT_MESSAGE)
     origin = repo.remote(name='origin')
     origin.push()
